1323d0e48982e00737727a3790955762d9324df3
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-08-05 02:29:46 +0300

    Add environment variables support for API URL

--- client/src/Baseapi.tsx
+++ client/src/Baseapi.tsx
@@ -4,7 +4,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 const BaseApi = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://telegram-mini-app-todo-list.onrender.com"
+        baseUrl: import.meta.env.VITE_API_URL || "https://telegram-mini-app-todo-list.onrender.com"
     }),
     endpoints: () => ({})
 });
